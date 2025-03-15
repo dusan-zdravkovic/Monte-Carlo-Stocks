c42970fde9fcddb4271a7e463e4545621379cce9
Dusan Zdravkovic <dusanz03@gmail.com>
2025-03-14 23:43:22 -0400

    new file

--- test.py
+++ test.py
@@ -1,23 +1,8 @@
-print("Hello Dusan")
+"Monte Carlo Stock simulation"
 
-# for i in range(20):
-#     print("Dusan Rocks" + 1)
-
-for i in range(20):
-    print(f"Dusan Rocks {i+1}")
-
-
-# watch
-
-i = 1
-while i <= 20:
-    print(f"Dusan Rocks {i}")
-    i += 1
-
-
-def print_message(times):
-    for i in range(1, times + 1):
-        print(f"Dusan Rocks {i}")
-
-
-print_message(10)
+import numpy as np
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+from pandas_datareader import data as pdr
