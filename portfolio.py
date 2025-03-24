922785ae484d2c1654893280d854d5c4d2c734b4
Dusan Zdravkovic <dusanz03@gmail.com>
2025-03-24 00:19:35 -0400

    created stock retrieval function

--- portfolio.py
+++ portfolio.py
@@ -1,5 +1,6 @@
 """
-Monte Carlo Stock simulation
+Monte Carlo Stock Simulation
+- Implement the Monte Carlo Method to simulate a stock portfolio
 March 23, 2025
 """
 
@@ -9,3 +10,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
 from pandas_datareader import data as pdr
+
+
+# Import Data
+def get_data(stocks, start, end):
+    stockData = pdr.get_data_yahoo(stocks, start, end)
+    stockData = stockData["Close"]  # only interested in daily changes
+    returns = stockData.pct_change()
+    meanReturns = returns.mean()
+    covMatrix = returns.cov()
+    return meanReturns, covMatrix
+
+
+stockList = ["CBA", "BHP", "TLS", "NAB", "WBC", "STO"]
+stocks = [stock + ".AX" for stock in stockList]
+endDate = dt.datetime.now()
+startDate = endDate - dt.timedelta(days=300)
+
+meanReturns, covMatrix = get_data(stocks, startDate, endDate)
+
+print(meanReturns)
