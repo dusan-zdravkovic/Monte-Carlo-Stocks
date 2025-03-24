350d6e3f98bcb7fcc333544a5e3ada044bfce68d
Dusan Zdravkovic <dusanz03@gmail.com>
2025-03-24 01:28:35 -0400

    stuck for now yahoo finance due to too many API calls

--- portfolio.py
+++ portfolio.py
@@ -9,12 +9,19 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
+
+# from pandas_datareader import data as pdr - yahoo fianance no longer works directly
+import yfinance as yf
 from pandas_datareader import data as pdr
+
+yf.pdr_override()  # override pandas_datareader with yfinance
 
 
 # Import Data
 def get_data(stocks, start, end):
     stockData = pdr.get_data_yahoo(stocks, start, end)
+    print(stockData.head())  # Check if data is retrieved properly
+
     stockData = stockData["Close"]  # only interested in daily changes
     returns = stockData.pct_change()
     meanReturns = returns.mean()
